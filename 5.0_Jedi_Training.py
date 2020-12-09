bfdc7fd17d988bcb903f567b88b73dfc08614c7e
lopezsantiagoger <lopezsantiagoger@urbandaleschools.com>
2020-12-09 10:35:41 -0600

    commitcamels

--- 5.0_Jedi_Training.py
+++ 5.0_Jedi_Training.py
@@ -1,4 +1,4 @@
-  #Sign your name:________________
+  #Sign your name:_Gerardo Lopez_______________
 
 '''
  1. Make the following program work.
@@ -14,7 +14,10 @@ for i in range(3):
   2. Write a Python program that will use a FOR loop to print the even
      numbers from 2 to 100, inclusive.
 '''
-
+x = 0
+for i in range(50):
+    x = x + 2
+    print(x)
 
 
 
@@ -24,7 +27,11 @@ for i in range(3):
      10 down to, and including, 0. Then print the words Blast off! Remember, use
      a WHILE loop, don't use a FOR loop.
 '''
-
+x = 10
+while x >= 0:
+    print(x)
+    x = x - 1
+print("Blast off!")
 
 
 
@@ -33,7 +40,9 @@ for i in range(3):
 '''
   4. Write a program that prints a random integer from 1 to 10 (inclusive).
 '''
-
+import random
+number = random.randint(1,10)
+print(number)
 
 
 
@@ -49,3 +58,23 @@ for i in range(3):
      if statements.
       
 '''
+total = 0
+pos = 0
+neg = 0
+zero = 0
+for i in range(7):
+    x = int(input("Enter a number: "))
+    total = total + x
+    if x > 0:
+        pos = pos + 1
+    elif x < 0:
+        neg = neg + 1
+    else:
+        zero = zero + 1
+print("Number of postive entries: ", pos)
+print("Number of negative entries: ", neg)
+print("Number of entries equal to zero", zero)
+
+print("The total is:", total)
+
+
